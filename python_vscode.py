f343b48c65ad7f303d76d68a768debf729cb28ce
JMahouna <jesse.supemir@gmail.com>
2025-01-06 11:40:30 +0100

    Real Time GUI

--- python_vscode.py
+++ python_vscode.py
@@ -1,12 +1,34 @@
 import random
+from tkinter import *
+from time import strftime
 
-print('your password')
+# ******* My code to generate my own message ***********
 
-char='12&é34ùmp^$*!:ml:;!/.?/§ùmp^µ£_-wxc>><<dà)=+1234MAPEN?SNFUTIOOOCNJSK'
+# print('your password')
 
-password=''
+# char='12&é34ùmp^$*!:ml:;!/.?/§ùmp^µ£_-wxc>><<dà)=+1234MAPEN?SNFUTIOOOCNJSK'
 
-for x in range(16) :
-    password+=random.choice(char)
+# password=''
 
-print(password)
+# for x in range(16) :
+#     password+=random.choice(char)
+
+# print(password)
+
+# ******** real-time clock GUI *********************
+
+myWindows = Tk() # initializing a windows
+myWindows.title('Real Time') 
+
+def time():
+    mytime = strftime('%H:%M:%S %p')
+    clock.config(text = mytime)
+    clock.after(1000, time)
+
+clock = Label(myWindows, font = ('arial',40,'bold'),
+              background= 'dark green',foreground = 'white')
+
+clock.pack(anchor = 'center')
+time()
+mainloop()
+
